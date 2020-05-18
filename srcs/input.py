c74bbe189aeb72f8ff8576104efea17a80388085
Papaiou <jacques.alzate@epitech.eu>
2020-05-18 10:43:36 +0200

    Implementing every commands for the input handdler. Final commit of this branch.

--- srcs/input.py
+++ srcs/input.py
@@ -13,25 +13,82 @@ def exit_input():
     print("Thanks a lot and see you soon !")
     return 0
 
+def add_question():
+    new_question = input("Which question do you wanna add ? : ")
+    print("new question added : " + new_question)
+    return input_start()
+
+def get_survey():
+    print("Here is the surveys you want")
+    return input_start()
+
+def survey_list():
+    print("Here are all the surveys")
+    return input_start()
+
+def survey_stats():
+    print("Here are the stats of the survey you want")
+    return input_start()
+
+def question_stats():
+    print("Here are the stats for the question you want")
+    return input_start()
+
+def new_survey_response():
+    print("Here is a new survey response")
+    return input_start()
+
+def get_survey_response():
+    print("Here are all the survey responses")
+    return input_start()
+
 def input_start():
     possibilities = {
-    "help": print_help,
-    "-h": print_help,
-    "h": print_help,
     "new survey": new_survey,
-    "new Survey": new_survey,
+    "get survey": get_survey,
+    "list survey": survey_list,
+    "survey list": survey_list,
+    "add question": add_question,
+    "survey stats": survey_stats,
+    "question stats": question_stats,
+    "survey statistics": survey_stats,
+    "question statistics": question_stats,
+    "new survey response": new_survey_response,
+    "get survey response": get_survey_response,
+
     "new": new_survey,
-    "-n": new_survey,
-    "n": new_survey,
+    "help": print_help,
     "exit": exit_input,
+    "list": survey_list,
+    "add": add_question,
+
+    "-h": print_help,
     "-e": exit_input,
+    "-l": survey_list,
+    "-ns": new_survey,
+    "-gs": get_survey,
+    "-a": add_question,
+    "-ss": survey_stats,
+    "-qs": question_stats,
+    "-nsr": new_survey_response,
+    "-gsr": get_survey_response,
+
+    "h": print_help,
     "e": exit_input,
+    "l": survey_list,
+    "ns": new_survey,
+    "gs": get_survey,
+    "a": add_question,
+    "ss": survey_stats,
+    "qs": question_stats,
+    "nsr": new_survey_response,
+    "gsr": get_survey_response,
     }
 
     request = input("what do you want ? : ")
     try:
         return possibilities[request]()
     except:
-        print("bad_input !")
+        print("bad_input ! Use \"help\" to see how to use it !")
         return input_start()
     return 0
